c8f5f807dab4de6d0c68be84a9588c9831026688
verissimo-sn <jose.neto@minu.co>
2022-02-09 08:27:45 -0300

    feat: added create clients useCase

---
+++ src/modules/clients/useCases/createClient/createClientUseCase.ts
@@ -0,0 +1,38 @@
+import { hash } from "bcrypt";
+import { prismaClient } from "../../../../database/prismaClient";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    const clientExists = await prismaClient.clients.findFirst({
+      where: { 
+        username: {
+          mode: 'insensitive'
+        }
+      }
+    });
+
+    if (clientExists) {
+      throw new Error('Client already exists');
+    }
+
+    const salt = 10;
+    const hashPassword = await hash(password, salt);
+
+    const newCLient = await prismaClient.clients.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    });
+
+    return {
+      id: newCLient,
+      username
+    }
+  }
+}
