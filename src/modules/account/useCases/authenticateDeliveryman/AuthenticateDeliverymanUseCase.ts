d63a3ee2a43718891e6e01df6e3623b65edeff19
verissimo-sn <jose.neto@minu.co>
2022-02-19 08:04:07 -0300

    feat(auth): added deliveryman authentication

---
+++ src/modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanUseCase.ts
@@ -0,0 +1,40 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+
+import { prismaClient } from "../../../../database/prismaClient";
+
+interface IAuthenticateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateDeliverymanUseCase {
+  async execute({ username, password }: IAuthenticateDeliveryman): Promise<string> {
+    const deliveryman = await prismaClient.deliveryman.findFirst({
+      where: {
+        username
+      }
+    });
+
+    if (!deliveryman) {
+      throw new Error('User or password invalid');
+    }
+
+    const passwordMatch = await compare(password, deliveryman.password);
+
+    if(!passwordMatch) {
+      throw new Error('User or password invalid');
+    }
+
+    const payload = {
+      username
+    }
+
+    const token = sign(payload, 'secret', {
+      subject: deliveryman.id,
+      expiresIn: '1d',
+    });
+
+    return token;
+  }
+}
--- src/routes/deliveryman.routes.ts
+++ src/routes/deliveryman.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { AuthenticateDeliverymanController } from '../modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateDeliverymanController } from '../modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const deliverymanRoutes = Router();
 
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
 deliverymanRoutes.post('/', createDeliverymanController.handle);
+deliverymanRoutes.post('/auth', authenticateDeliverymanController.handle);
 
 export { deliverymanRoutes }
+
