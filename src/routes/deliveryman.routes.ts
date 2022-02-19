dbe71692d1fde0e094f144db1627a8673bf3ebbc
verissimo-sn <jose.neto@minu.co>
2022-02-19 07:52:16 -0300

    feat(deliveryman): added deliveryman routes

---
+++ src/routes/deliveryman.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { CreateDeliverymanController } from '../modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
+
+const deliverymanRoutes = Router();
+
+const createDeliverymanController = new CreateDeliverymanController();
+
+deliverymanRoutes.post('/', createDeliverymanController.handle);
+
+export { deliverymanRoutes }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { clientRoutes } from './clients.routes';
+import { deliverymanRoutes } from './deliveryman.routes';
 
 const router = Router();
 
 router.use('/clients', clientRoutes);
+router.use('/deliveryman', deliverymanRoutes);
 
 export { router }
