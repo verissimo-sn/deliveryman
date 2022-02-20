277cb9d29e1e7b1c44fdd9091d405484392d3ffe
verissimo-sn <jose.neto@minu.co>
2022-02-20 07:45:52 -0300

    feat(delivey): added auth middleware in findAllAvailable route

--- src/routes/delivery.routes.ts
+++ src/routes/delivery.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { ensureAuthenticateClient } from '../middlewares/ensureAuthenticateClient';
+import { ensureAuthenticateDeliveryman } from '../middlewares/ensureAuthenticateDeliveryman';
 import { CreateDeliveryController } from '../modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { FindAllAvailableController } from '../modules/deliveries/useCases/findAllAvailable/FindAllAvailableController';
 
@@ -9,7 +10,7 @@ const createDeliveryController = new CreateDeliveryController();
 const findAllAvailableController = new FindAllAvailableController();
 
 deliveryRoutes.post('/', ensureAuthenticateClient, createDeliveryController.handle);
-deliveryRoutes.get('/available', findAllAvailableController.handle);
+deliveryRoutes.get('/available', ensureAuthenticateDeliveryman, findAllAvailableController.handle);
 
 export { deliveryRoutes }
 
